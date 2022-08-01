715ce507f56323a8823853b1c17e5e495346bc84
lilian <lilian.wangechi@student.moringaschool.com>
2022-08-01 20:28:34 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+document.querySelector("#main").remove();
+
+const newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory");
+
+// newHeader.("YOUR-NAME is the champion")
+newHeader.innerHTML="YOUR-NAME is the champion";
